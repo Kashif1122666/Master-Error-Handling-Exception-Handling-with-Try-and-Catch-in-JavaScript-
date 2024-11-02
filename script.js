9f115cc6a171f4e70a3eddd037ed52db6ec6074f
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2024-11-02 12:15:14 -0700

    Add files via upload
    Master Error Handling: Exception Handling with Try and Catch in JavaScript!

---
+++ script.js
@@ -0,0 +1,14 @@
+
+// chapter 87- Try and catch
+
+
+// USING TRY CATCH BLOCK 
+
+
+try{
+    var greeting = "Hello world!";
+    aler(greeting);
+}catch(error){
+      alert(error);
+      alert("There was a problem!");//we can use our own output for it
+}
